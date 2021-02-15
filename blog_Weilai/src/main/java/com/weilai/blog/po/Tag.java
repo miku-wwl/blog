0fad2554f7ba1cf82cdfac053cff36d311870f65
miku-wwl <38804125+miku-wwl@users.noreply.github.com>
2021-02-15 13:21:31 +1100

    实体类构建

---
+++ blog_Weilai/src/main/java/com/weilai/blog/po/Tag.java
@@ -0,0 +1,55 @@
+package com.weilai.blog.po;
+
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "t_tag")
+public class Tag {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private List<Blog> bolgs = new ArrayList<>();
+
+    public Tag(){
+
+    }
+
+    public List<Blog> getBolgs() {
+        return bolgs;
+    }
+
+    public void setBolgs(List<Blog> bolgs) {
+        this.bolgs = bolgs;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ blog_Weilai/src/test/java/com/weilai/blog/po/BlogTest.java
@@ -0,0 +1,6 @@
+package com.weilai.blog.po;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BlogTest {
+  
+}
