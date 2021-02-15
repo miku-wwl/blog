8f83c0f61ed681beb53895f78d328f69505e0a14
miku-wwl <38804125+miku-wwl@users.noreply.github.com>
2021-02-15 22:03:11 +1100

    2021/02/15

---
+++ blog_Weilai/src/main/java/com/weilai/blog/dao/TypeRepository.java
@@ -0,0 +1,8 @@
+package com.weilai.blog.dao;
+
+import com.weilai.blog.po.Type;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TypeRepository  extends JpaRepository<Type, Long> {
+
+}
---
+++ blog_Weilai/src/main/java/com/weilai/blog/service/TypeService.java
@@ -0,0 +1,19 @@
+package com.weilai.blog.service;
+
+import com.weilai.blog.po.Type;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface TypeService {
+    Type saveType(Type type);
+
+    Type getType(Long id);
+
+    Page<Type> listType(Pageable pageable);
+
+    Type updateType(Long id, Type type);
+
+    void deleteType(Long id);
+
+
+}
