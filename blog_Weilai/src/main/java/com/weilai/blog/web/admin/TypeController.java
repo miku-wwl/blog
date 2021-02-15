a3f1076c483f57c2c84710adf5cd66434d98d733
miku-wwl <38804125+miku-wwl@users.noreply.github.com>
2021-02-15 23:23:52 +1100

    2021/02/15

--- blog_Weilai/src/main/java/com/weilai/blog/web/admin/TypeController.java
+++ blog_Weilai/src/main/java/com/weilai/blog/web/admin/TypeController.java
@@ -1,5 +1,6 @@
 package com.weilai.blog.web.admin;
 
+import com.weilai.blog.po.Type;
 import com.weilai.blog.service.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +9,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,9 +20,26 @@ public class TypeController {
     private TypeService typeService;
 
     @GetMapping("/types")
-    public String types(@PageableDefault(size = 10,sort = {"id"},direction = Sort.Direction.DESC)
+    public String types(@PageableDefault(size = 30,sort = {"id"},direction = Sort.Direction.DESC)
                                 Pageable pageable, Model model) {
         model.addAttribute("page",typeService.listType(pageable));
         return "admin/types";
+    }
+
+    @GetMapping("/types/input")
+    public  String input(){
+        return "admin/types-input";
+    }
+
+    @PostMapping("/types")
+    public String post(Type type){
+        Type t = typeService.saveType(type);
+        if (t == null){
+
+        }else{
+
+        }
+
+        return "redirect:/admin/types";
     }
 }
